4418506d92c9317b24c9764bb7f08ae90dce3f8a
aayusharya1 <aayusharya07@gmail.com>
2020-09-29 14:25:25 +0530

    [Aayush]add.Code for random no. btw 1 to 6

---
+++ SnakeLadderProj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int num=r.Next(1,7);
+
+        }
+    }
+}
