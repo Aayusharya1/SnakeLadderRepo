aff5372fa6c96c126d1d87a5cea6f6ff3b413b47
aayusharya1 <aayusharya07@gmail.com>
2020-09-30 05:39:54 +0530

    [Aayush] add. Code for snake, ladder and noplay

--- SnakeLadderProj/Program.cs
+++ SnakeLadderProj/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
+using System.Collections;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 
 namespace SnakeLadder
 {
     class Program
     {
+        public const int start = 0;
+        public const int end = 100;
+        public const int no_play = 0;
+        public const int snake = 1;
+        public const int ladder = 2;
+ 
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int num=r.Next(1,7);
+            Console.WriteLine("Welcome to snakes and ladder game.");
+            
 
         }
+
+        static int DiceRoll() 
+        {
+            Random r = new Random();
+            int dice_num = r.Next(1, 7);
+            return dice_num;
+        }
+
+        static int MoveDirection(int player_position, int dice_num) 
+        {
+            Random r = new Random();
+            int move_no = r.Next(0,3);
+
+            switch (move_no)
+            {
+                case no_play:
+                    break:
+
+                case snake:
+                    if (player_position > dice_num) player_position = player_position - dice_num;
+                    else player_position = 0;
+                    break;
+
+                case ladder:
+                    if (player_position + dice_num <= 100) player_position = player_position + dice_num;
+                    break;
+
+            }
+
+            return player_position;
+        
+        }
     }
+
 }
